e08b1c629ce7739c44f980c131ad652348d69a44
Andrey Kucherenko <andykucherenko@gmail.com>
2023-05-06 15:59:40 +0300

    First task

--- src/test/java/ru/netology/rest/RegistrationTest.java
+++ src/test/java/ru/netology/rest/RegistrationTest.java
@@ -1,25 +1,38 @@
 package ru.netology.rest;
 
 import com.codeborne.selenide.Configuration;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.openqa.selenium.Keys;
 
 import java.time.Duration;
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
 
-import static com.codeborne.selenide.Condition.appear;
-import static com.codeborne.selenide.Condition.visible;
-import static com.codeborne.selenide.Selenide.*;
+import static com.codeborne.selenide.Selenide.$;
+import static com.codeborne.selenide.Selenide.open;
+import static com.codeborne.selenide.Condition.text;
+
+
 
 public class RegistrationTest {
 
-    @Test
-    void shouldRegisterByAccountNumberDOMModification() {
-        Configuration.holdBrowserOpen = true;
+    @BeforeEach
+    void setUp() {
         open("http://localhost:9999/");
-        $("a").click();
-        $("[type=text]").setValue("4055 0100 0123 4613 8564");
-        $("[type-tel]").setValue("+7 999 921 33 11");
+        $("[data-test-id='date'] input").doubleClick().sendKeys(Keys.BACK_SPACE);
+        Configuration.holdBrowserOpen = true;
+    }
+
+    @Test
+    public void sendFormSuccessfulTest() {
+        String myDate = LocalDate.now().plusDays(4).format(DateTimeFormatter.ofPattern("dd.MM.yyyy"));
+        $("[data-test-id='city'] input").setValue("Санкт-Петербург");
+        $("[data-test-id='date'] input").sendKeys(myDate);
+        $("[data-test-id='name'] input").setValue("Иван Петров-Сидоров");
+        $("[data-test-id='phone'] input").setValue("+79012345678");
+        $("[data-test-id='agreement']").click();
         $(".button__text").click();
-        $x("//p[contains(text(), 'Успешная авторизация')]").shouldBe(visible, Duration.ofMillis(8000));
-        $x("//*[contains(text(), 'кабинет')]").should(appear, Duration.ofSeconds(6));
+        $("[data-test-id='notification']").should(text("Встреча успешно забронирована на " + myDate), Duration.ofSeconds(15));
     }
 }
